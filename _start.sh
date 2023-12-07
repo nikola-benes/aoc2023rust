0282dc2127a14d3c30de84c6dc426480c49dc6b4
Nikola Beneš <nikola.benes@gmail.com>
2023-12-07 09:10:42 +0100

    Add start-of-the-day script

---
+++ _start.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+Y=2023
+C=  # Insert session cookie here.
+A="nikola.benes@gmail.com via curl"
+
+if [ "$C" = "" ]; then
+	echo "Missing session cookie."
+	exit
+fi
+
+DAY="$1"
+ZDAY=$(printf '%02d' "$1")
+
+if [ "$DAY" = "" ]; then
+	echo "Usage: $0 <day_number>"
+	exit
+fi
+
+cat <<END >> Cargo.toml
+
+[[bin]]
+name = "$ZDAY"
+path = "src/$ZDAY.rs"
+END
+
+cat <<END > "src/$ZDAY.rs"
+mod aoc;
+use aoc::*;
+
+fn main() {
+
+    // println!("{:?}", x);
+}
+END
+
+curl "https://adventofcode.com/$Y/day/$DAY/input" \
+	--cookie "session=$C" --user-agent "$A" > "input$ZDAY"
