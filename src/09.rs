0fa17689fd4806d2516c038e14939a7023eee753
Nikola Beneš <nikola.benes@gmail.com>
2023-12-09 14:40:32 +0100

    Day 9: Reduce space complexity

--- src/09.rs
+++ src/09.rs
@@ -5,14 +5,20 @@ fn prev_value(mut nums: Vec<i64>) -> i64 {
     next_value(nums)
 }
 
-fn next_value(nums: Vec<i64>) -> i64 {
+fn next_value(mut nums: Vec<i64>) -> i64 {
+    next_value_impl(nums.as_mut_slice());
+    nums.into_iter().sum_()
+}
+
+fn next_value_impl(nums: &mut [i64]) {
     if nums.iter().all(|&n| n == 0) {
-        return 0;
+        return;
     }
-    nums.last().unwrap()
-        + next_value(
-            nums.iter().zip(nums.iter().skip(1)).map_v(|(&a, &b)| b - a),
-        )
+    let last = nums.len() - 1;
+    for i in 0..last {
+        nums[i] = nums[i + 1] - nums[i];
+    }
+    next_value_impl(&mut nums[..last]);
 }
 
 fn main() {
