d2cd42b6e2367014372161f633f7ffe388dd22a2
Nikola Beneš <nikola.benes@gmail.com>
2023-12-19 12:10:58 +0100

    Day 18: Change to a better solution

--- src/18.rs
+++ src/18.rs
@@ -1,11 +1,23 @@
 use aoc::*;
-use std::collections::BTreeMap;
 
 const DIRS: [(i64, i64); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 const N: u8 = 0b0001;
 const E: u8 = 0b0010;
 const S: u8 = 0b0100;
 const W: u8 = 0b1000;
+
+// Pick's theorem:
+//   A = i + b / 2 - 1
+// where
+//   A = area of polygon with integer vertex coordinates
+//   i = interior points with integer coordinates
+//   b = boundary points with integer coordinates
+//
+// thus: i = A - b / 2 + 1
+//
+// we need to compute b + i = A + b / 2 + 1
+// to compute A, we sum oriented triangles (0, 0) -- (x, y) -- (next_x, next_y)
+// each such area is (x * next_y - next_x * y) / 2
 
 fn normalise_dir(d: char) -> u8 {
     match d {
@@ -21,39 +33,19 @@ fn normalise_dir(d: char) -> u8 {
     }
 }
 
-fn opp_dir(d: u8) -> u8 {
-    if d & (N | S) != 0 {
-        d ^ (N | S)
-    } else {
-        d ^ (E | W)
-    }
-}
-
 fn solve(plan: &Vec<(u8, i64, String)>) -> i64 {
-    let mut dig = BTreeMap::new();
-    let (mut y, mut x) = (0, 0);
-    for &(dir, len, _) in plan {
-        let (dy, dx) = DIRS[dir.trailing_zeros() as usize];
-        for _ in 0..len {
-            *dig.entry((y, x)).or_insert(0) |= dir;
-            y += dy;
-            x += dx;
-            *dig.entry((y, x)).or_insert(0) |= opp_dir(dir);
-        }
-    }
-
-    let mut total = dig.len() as i64;
-    let mut inside = 0;
-    let mut last = 0;
-    for (&(_, x), &tile) in dig.iter() {
-        if inside != 0 {
-            total += x - last - 1
-        }
-        inside ^= tile & (N | S);
-        last = x;
-    }
-
-    total
+    let mut last = (0, 0);
+    let twice_area = plan
+        .map(|&(dir, len, _)| {
+            let (dy, dx) = DIRS[dir.trailing_zeros() as usize];
+            let (ly, lx) = last;
+            let (ny, nx) = (ly + dy * len, lx + dx * len);
+            last = (ny, nx);
+            lx * ny - nx * ly
+        })
+        .sum_();
+    let boundary = plan.map(|&(_, len, _)| len).sum_();
+    twice_area / 2 + boundary / 2 + 1
 }
 
 fn main() {
